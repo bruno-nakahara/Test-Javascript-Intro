3ce9660ccf66084d9d4052c4a04306e624012b08
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2022-02-26 11:36:25 -0300

    configure gitignore and apply prettier

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,27 +1,27 @@
-const { sum } = require("./calcuator")
+const { sum } = require('./calcuator');
 
 it('sum 2 and 2 and the result must e 4', () => {
-    expect(sum(2, 2)).toBe(4)
+  expect(sum(2, 2)).toBe(4);
 });
 
 it('sum 2 and 2 even if one of them is a string and the result must e 4', () => {
-    expect(sum('2', '2')).toBe(4)
+  expect(sum('2', '2')).toBe(4);
 });
 
 it('should throw an error if what is provided to the method cannot e summed', () => {
-    expect(() => {
-        sum('', '2')
-    }).toThrowError()
+  expect(() => {
+    sum('', '2');
+  }).toThrowError();
 
-    expect(() => {
-        sum([2, 2])
-    }).toThrowError()
+  expect(() => {
+    sum([2, 2]);
+  }).toThrowError();
 
-    expect(() => {
-        sum({})
-    }).toThrowError()
+  expect(() => {
+    sum({});
+  }).toThrowError();
 
-    expect(() => {
-        sum()
-    }).toThrowError()
+  expect(() => {
+    sum();
+  }).toThrowError();
 });
